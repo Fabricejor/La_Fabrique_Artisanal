ce6f5a9c6bbcb54cbce47e4d1a67c1d05e44d426
Fabricejor <Fabricejordan2001@gmail.com>
2024-09-27 02:27:28 +0000

    Ajout navbar+ banner image zoom

--- components/main/Banner.tsx
+++ components/main/Banner.tsx
@@ -4,20 +4,20 @@ import Image from 'next/image'
 
 export default function Banner() {
     return (
-        <header className='w-full h-full mt-28 bg-white flex flex-row items-center gap-10 justify-between px-10'>
-            <div className='FISRTTEXT flex flex-col w-[40%]'>
+        <header className='w-full h-full mt-14 flex flex-row items-center gap-10 justify-between px-10'>
+            <div className='FISRTTEXT flex flex-col basis-2/6'>
                 <h1 className='text-black text-3xl font-normal capitalize leading-7'>LA Fabrique Artisanal Présentation</h1>
                 <h3 className='text-black text-1xl text-justify font-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio praesentium, mollitia enim consequuntur autem quod. Deleniti, accusamus dolor! Doloremque, quisquam! Officia, dolore quaerat magni accusamus maiores magnam facilis exercitationem!</h3>
                 <button type='button'> Shop now!</button>
             </div>
-            <div className='BIGIMAG w-full flex flex-row justify-between'>
-                <Image src="/banner.jpeg" width={100} height={100} className='w-[100%]' alt='banner image' />
-                <div className='OHTERIMAGE w-full flex flex-col'>
-                    <Image src="/sacrouge.jpeg" width={50} height={50} alt='banner image' />
-                    <Image src="/sacnoir.jpeg" width={50} height={50} alt='banner image' />
-                </div>
+            <div className='BIGIMAG w-full flex flex-row   overflow-hidden bg-cover bg-no-repeat items-center justify-between basis-2/6 '>
+                <Image src="/banner.jpeg" width={200} height={200} className='w-full img1 bg-black  rounded object-cover transition duration-300 ease-in-out hover:scale-110' alt='banner image' />
+              
             </div>
-
+            <div className='OHTERIMAGE w-full flex flex-col items-center justify-evenly basis-2/6 '>
+                    <Image src="/sacrouge.jpeg" width={200} height={200}className='w-[100%] rounded mb-6' alt='banner image' />
+                    <Image src="/sacnoir.jpeg" width={200} height={200} className='w-[100%] rounded ' alt='banner image' />
+                </div>
         </header>
     )
 }
