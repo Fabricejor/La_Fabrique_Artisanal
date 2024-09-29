8a2b2005f790341a3d5ebc83ec82209884d639fb
Fabricejor <Fabricejordan2001@gmail.com>
2024-09-29 21:42:13 +0000

    tentative responsivité

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React,{useState} from 'react'
+import React from 'react'
 import { CiSearch ,CiUser ,CiHeart} from "react-icons/ci";
 import { HiOutlineShoppingBag } from "react-icons/hi";
 import Link from 'next/link';
